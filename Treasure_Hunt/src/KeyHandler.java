3f13340676dea5039480870355200b6cfcf2d883
vnguyen21 <vnguyen21@worcester.edu>
2024-12-09 20:46:24 -0500

    Updated KeyHandler.java
    Arrow keys for movement have been added.

--- Treasure_Hunt/src/KeyHandler.java
+++ Treasure_Hunt/src/KeyHandler.java
@@ -1,51 +1,78 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class KeyHandler implements KeyListener {
+public class KeyHandler implements KeyListener{
 
-    // Boolean flags to track which keys are pressed
     public boolean upPressed, downPressed, leftPressed, rightPressed;
-
-    @Override
+    @Override //Not used
     public void keyTyped(KeyEvent e) {
-        // Not used
+       
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        // Support both WASD and Arrow Keys
-        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+    // W, A, S, D keys for movement
+        if(code == KeyEvent.VK_W){
             upPressed = true;
         }
-        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+        if(code == KeyEvent.VK_S){
             downPressed = true;
         }
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+        if(code == KeyEvent.VK_A){
             leftPressed = true;
         }
-        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+        if(code == KeyEvent.VK_D){
+            rightPressed = true;
+        }
+
+    // Arrow keys for movement 
+        if (code == KeyEvent.VK_UP) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_DOWN) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_RIGHT) {
             rightPressed = true;
         }
     }
-
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
 
-        // Support both WASD and Arrow Keys
-        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+        // Reset key press flags when keys are released
+        if(code == KeyEvent.VK_W){
             upPressed = false;
         }
-        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+        if(code == KeyEvent.VK_S){
             downPressed = false;
         }
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+        if(code == KeyEvent.VK_A){
             leftPressed = false;
         }
-        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+        if(code == KeyEvent.VK_D){
+            rightPressed = false;
+        }
+
+        // Arrow keys
+        if (code == KeyEvent.VK_UP) {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_DOWN) {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_RIGHT) {
             rightPressed = false;
         }
     }
+
 }
+
